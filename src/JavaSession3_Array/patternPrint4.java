0b26044c306f2fea801e91d340f8fe8767fbf2fa
kanishkaa <kanishkaas2007@gmail.com>
2026-03-03 14:34:34 +0530

    init commit

---
+++ src/JavaSession3_Array/patternPrint4.java
@@ -0,0 +1,30 @@
+package JavaSession3_Array;
+
+import java.util.Scanner;
+
+public class patternPrint4 {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter number: ");
+            int n = sc.nextInt();
+
+            for(int i = 0; i < n; i++) {
+
+                for(int j = 0; j < i; j++) {
+                    System.out.print("  ");
+                }
+
+                for(int k = i; k < n; k++) {
+                    System.out.print("* ");
+                }
+
+                System.out.println();
+            }
+
+            sc.close();
+        }
+    }
+
